2ea72e060911cf20a834c6af8adba51f0cff9584
anwardv <anwardv@gmail.com>
2020-08-17 11:42:55 +0000

    first commit

---
+++ src/main/java/org/sid/ServeurGestionStockPRoduitsApplication.java
@@ -0,0 +1,13 @@
+package org.sid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionStockPRoduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionStockPRoduitsApplication.class, args);
+	}
+
+}
